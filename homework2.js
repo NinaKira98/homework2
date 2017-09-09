890f6d857c56a11831834ebfa147ba884debe4dc
NinaKira <nina.kirakosyan.98@gmail.com>
2017-09-09 18:53:11 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,23 @@
+alert ("This is CS110 HW2")
+
+const first= 7
+const second= 10
+
+if (first > second) {
+	console.log (first - second)
+	console.log ("The first number is greater then the second number")
+}
+else if (first < second) {
+	console.log (second - first)
+	console.log ("The Second number is greater than the first number")
+}
+else {
+	console.log ("Those numbers are equal")
+}
+ 
+ console.log ("Another example:")
+ const height = 1.7
+ const weight = 51
+ console.log ("This number is the BMI")
+ console.log (weight/(height*height))
+ 
